f55c3e7d52436643a4556fdf9e9767606f3e8d4a
serratomayra <46508815+serratomayra@users.noreply.github.com>
2019-12-14 18:43:16 -0800

    gameOver

--- Universe-Game/Assets/Timer/Countdown.cs
+++ Universe-Game/Assets/Timer/Countdown.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Countdown : MonoBehaviour
 {
-
     float currentTime = 0f;
-    float startingTime = 30f;
+    float startingTime = 10f; //can change to whatever number you want it to start at
 
     [SerializeField] Text countdownText;
 
@@ -18,7 +18,6 @@ public class Countdown : MonoBehaviour
 
     void Update()
     {
-
         //counts down by 1
         currentTime -= 1 * Time.deltaTime;
         countdownText.text = currentTime.ToString("0"); //helps countdown whole numbers
@@ -27,6 +26,12 @@ public class Countdown : MonoBehaviour
         if (currentTime <= 0)
         {
             currentTime = 0;
+        }
+
+        //changes to main menu scene
+        if (currentTime == 0)
+        {
+            SceneManager.LoadScene(2);
         }
     }
 }
